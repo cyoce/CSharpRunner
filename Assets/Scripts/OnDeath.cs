5fc2cce97f70f310c7ff290fe837e1a7419661fd
JBuck55 <joshuabuckmann55@gmail.com>
2023-11-30 17:13:03 -0800

    new death screen

---
+++ Assets/Scripts/OnDeath.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class OnDeath : MonoBehaviour
+{
+    public int onDeath;
+
+    public void Dies()
+    {
+        
+    }
+
+}
