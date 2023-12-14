209940ea0af9bb725548456fc47fc0e269fbad63
cyoce <masterryangp@gmail.com>
2023-12-13 23:10:21 -0800

    cleanup

--- Assets/Scripts/BunnyScripts/Bunny.cs
+++ Assets/Scripts/BunnyScripts/Bunny.cs
@@ -29,7 +29,6 @@ public class Bunny : MonoBehaviour {
             activationTime = Time.time;
             trail = bunnyCounter.GetComponent<Trail>();
             player = bunnyCounter.GetComponent<PlayerControl>();
-            //baseDelay += 0.1f;
             delay = baseDelay;
         } else if(other.tag == "Jump") {
             if(jumpTriggers == 0) {
@@ -57,48 +56,15 @@ public class Bunny : MonoBehaviour {
             --jumpTriggers;
         }
     }
-    private void OnDrawGizmos() {
-        //Handles.Label(transform.position, coeff.ToString("0.00"));
-    }
     private void FixedUpdate() {
         if(following && Time.time - activationTime >= delay) {
 
             float dist = transform.position.x - player.transform.position.x;
-            //coeff = 1 + Mathf.Pow(dist - 1, -1);
             coeff = Mathf.Max(2 / (Mathf.Pow(1.5f, (dist+1)/2) + 1) - 1, 0);
             Vector3 offset = transform.position + Vector3.up * 0.5f;
             rb.AddForceAtPosition(Vector3.right * accel, offset);
             rb.AddForce(Vector3.left * rb.velocity.x * coeff);
             rb.velocity *= Mathf.Pow(coeff, Time.fixedDeltaTime);
-            if(rb.velocity.x > player.speed) {
-                //rb.velocity = new(player.speed, rb.velocity.y);
-            }
-            /*
-            TrailPoint dest = trail.GetPosition(Time.time - delay);
-            //transform.position = dest.position;
-            GetComponent<Rigidbody2D>().velocity = dest.velocity;
-            */
-        }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision) {
-        if(collision.gameObject.TryGetComponent<Bunny>(out Bunny other)) {
-            if(other.transform.position.x > transform.position.x) {
-                /*activationTime = Time.time;
-                delay = 1f;*/
-                // + 0.4f - baseDelay;
-                //other.GetComponent<Rigidbody2D>().AddForce(Vector3.up * accel, ForceMode2D.Impulse);
-                
-             
-                
-
-            }
-            float impulse = 100f;
-            //rb.AddForce(collision.relativeVelocity * impulse, ForceMode2D.Impulse);
-        } else if(collision.gameObject.layer == 3){
-            /*if(collision.GetContact(0).normal.y > 0) {
-                rb.velocity = new(rb.velocity.x, 0);
-            }*/
         }
     }
 
